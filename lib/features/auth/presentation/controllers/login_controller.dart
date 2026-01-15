0353175aa25d30d04a49f538b2427bbf8e6ed6fb
Rafael Costa <rafaeladriancordeirocosta@gmail.com>
2026-01-15 00:08:12 -0300

    [refactor] using the secure storage to mantain or delete secure info

--- lib/features/auth/presentation/controllers/login_controller.dart
+++ lib/features/auth/presentation/controllers/login_controller.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/foundation.dart';
 
-import 'package:shared_preferences/shared_preferences.dart';
-
 import 'package:aura/core/routes/app_routes.dart';
+import 'package:aura/core/services/local_storage_service.dart';
 import 'package:aura/features/auth/data/models/auth_response_model.dart';
 import 'package:aura/features/auth/data/repositories/auth_repository.dart';
 
@@ -10,6 +9,7 @@ class LoginController extends ChangeNotifier {
   final AuthRepository repository;
   bool isLoading = false;
   String? _currentUserRole;
+  final LocalStorageService _storage = LocalStorageService();
 
   LoginController(this.repository);
 
@@ -22,8 +22,21 @@ class LoginController extends ChangeNotifier {
         email,
         password,
       );
-      final prefs = await SharedPreferences.getInstance();
-      await prefs.setString('jwt_token', response?.token ?? '');
+
+      if (response != null) {
+        if (response.token.isNotEmpty) {
+          await _storage.saveSecure(
+            LocalStorageService.keyJwtToken,
+            response.token,
+          );
+        }
+        await _storage.saveSecure(
+          LocalStorageService.keyUserRole,
+          response.role,
+        );
+
+        _currentUserRole = response.role;
+      }
 
       return response;
     } catch (e) {
@@ -52,8 +65,7 @@ class LoginController extends ChangeNotifier {
 
       String role = _currentUserRole ?? '';
       if (role.isEmpty) {
-        final prefs = await SharedPreferences.getInstance();
-        role = prefs.getString('user_role') ?? '';
+        role = await _storage.readSecure('user_role') ?? '';
       }
 
       if (role == 'OWNER') {
