165196cd9658f593d61dafbc1b54a8a445bb8521
Rafael Costa <rafaeladriancordeirocosta@gmail.com>
2026-01-15 00:00:02 -0300

    [feat] integrated dio client to call the api, and use secure storage to keep auth keys

--- lib/core/client/dio_client.dart
+++ lib/core/client/dio_client.dart
@@ -1,38 +1,52 @@
+import 'package:aura/core/routes/app_routes.dart';
+import 'package:aura/core/services/local_storage_service.dart';
+import 'package:aura/main.dart';
 import 'package:dio/dio.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 class DioClient {
   late Dio dio;
+  final LocalStorageService _storage = LocalStorageService();
 
   DioClient() {
     dio = Dio(
       BaseOptions(
-        baseUrl: 'http://localhost:8090',
-        connectTimeout: const Duration(seconds: 5),
-        receiveTimeout: const Duration(seconds: 3),
+        baseUrl: 'http://192.168.0.104:8090',
+        connectTimeout: const Duration(seconds: 10),
+        receiveTimeout: const Duration(seconds: 10),
       ),
     );
 
     dio.interceptors.add(
       InterceptorsWrapper(
         onRequest: (options, handler) async {
-          final prefs = await SharedPreferences.getInstance();
-          final token = prefs.getString('jwt_token');
-          if (token != null) {
+          final token = await _storage.readSecure(
+            LocalStorageService.keyJwtToken,
+          );
+          if (token != null && token.isNotEmpty) {
             options.headers['Authorization'] = 'Bearer $token';
           }
           return handler.next(options);
         },
 
         onError: (DioException e, handler) async {
-          if (e.response?.statusCode == 401 || e.response?.statusCode == 403) {
-            final prefs = await SharedPreferences.getInstance();
-            await prefs.remove('jwt_token');
+          final statusCode = e.response?.statusCode;
+          final bool ignoreAuthInterceptor =
+              e.requestOptions.extra['ignoreAuth'] == true;
+          if ((statusCode == 401 || statusCode == 403) &&
+              !ignoreAuthInterceptor) {
+            await _storage.clearAllSecure();
+
+            navigatorKey.currentState?.pushNamedAndRemoveUntil(
+              AppRoutes.login,
+              (route) => false,
+            );
           }
+
           return handler.next(e);
         },
       ),
     );
+
     dio.interceptors.add(
       LogInterceptor(
         requestHeader: true,
