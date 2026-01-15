c331dc6c07afe7eed458a2f078c9e1f0916922f4
Rafael Costa <rafaeladriancordeirocosta@gmail.com>
2026-01-15 00:02:22 -0300

    [add] user role model

---
+++ lib/features/auth/data/models/user_role_model.dart
@@ -0,0 +1,28 @@
+// ignore_for_file: constant_identifier_names
+enum UserRole {
+  ADMIN,
+  OWNER,
+  USER;
+
+  static UserRole fromString(String? value) {
+    return UserRole.values.firstWhere(
+      (e) => e.name == (value?.toUpperCase()),
+      orElse: () => UserRole.USER,
+    );
+  }
+}
+
+// --- POLICY / PERMISSIONS EXTENSION ---
+extension RoleCapabilities on UserRole {
+  bool get canViewSensitiveData {
+    return this == UserRole.ADMIN || this == UserRole.OWNER;
+  }
+
+  bool get canEditCompanySettings {
+    return this == UserRole.ADMIN || this == UserRole.OWNER;
+  }
+
+  bool get canManageUsers {
+    return this == UserRole.ADMIN;
+  }
+}
