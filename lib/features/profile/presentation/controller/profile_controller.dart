ce8e09f08796fcf6fa51775d7a4eb0fab6de4899
Rafael Costa <rafaeladriancordeirocosta@gmail.com>
2026-01-24 17:52:52 -0300

    [refactor] update the role is if needed

--- lib/features/profile/presentation/controller/profile_controller.dart
+++ lib/features/profile/presentation/controller/profile_controller.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 
+import 'package:aura/core/services/local_storage_service.dart';
+import 'package:aura/features/auth/data/models/user_role_model.dart';
 import 'package:aura/features/profile/data/model/user_profile_model.dart';
 import 'package:aura/features/profile/data/repository/profile_repository.dart';
 
 class ProfileController extends ChangeNotifier {
   final ProfileRepository _repository;
+  final LocalStorageService _storage = LocalStorageService();
 
   UserProfileModel? user;
+
   bool isLoading = false;
   String? error;
 
@@ -18,9 +22,23 @@ class ProfileController extends ChangeNotifier {
     notifyListeners();
 
     try {
-      user = await _repository.getCurrentUserProfile();
+      UserProfileModel apiUser = await _repository.getCurrentUserProfile();
+      final String? savedRoleString = await _storage.readSecure(
+        LocalStorageService.keyUserRole,
+      );
+      final UserRole savedRole = UserRole.fromString(savedRoleString);
+      UserRole finalRole = savedRole;
+      if (apiUser.role != UserRole.USER && apiUser.role != savedRole) {
+        finalRole = apiUser.role;
+        await _storage.saveSecure(
+          LocalStorageService.keyUserRole,
+          finalRole.name,
+        );
+      }
+
+      user = apiUser.copyWith(role: finalRole);
     } catch (e) {
-      error = e.toString();
+      rethrow;
     } finally {
       isLoading = false;
       notifyListeners();
