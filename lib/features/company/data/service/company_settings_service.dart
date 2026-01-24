ed6fc7f5af130c0e051578b96cdcc3aa9421a15c
Rafael Costa <rafaeladriancordeirocosta@gmail.com>
2026-01-24 17:44:21 -0300

    [refactor] changed post to put  http method

--- lib/features/company/data/service/company_settings_service.dart
+++ lib/features/company/data/service/company_settings_service.dart
@@ -6,6 +6,6 @@ class CompanySettingsApiService {
   CompanySettingsApiService(this._dio);
 
   Future<Response> saveMySettings(Map<String, dynamic> settings) async {
-    return await _dio.post('/companies/current/settings', data: settings);
+    return await _dio.put('/companies/current/settings', data: settings);
   }
 }
