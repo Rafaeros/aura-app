26ce3cb94e84062ba029753a8ce9679f4c316bb6
Rafael Costa <rafaeladriancordeirocosta@gmail.com>
2026-01-24 17:41:49 -0300

    [feat] convert user model from token function

--- lib/features/auth/data/models/user_model.dart
+++ lib/features/auth/data/models/user_model.dart
@@ -1,4 +1,5 @@
 import 'package:aura/features/auth/data/models/company_model.dart';
+import 'package:jwt_decoder/jwt_decoder.dart';
 
 class UserModel {
   final int id;
@@ -6,6 +7,7 @@ class UserModel {
   final String email;
   final String role;
   final CompanyModel company;
+  final String? token;
 
   UserModel({
     required this.id,
@@ -13,6 +15,7 @@ class UserModel {
     required this.email,
     required this.role,
     required this.company,
+    this.token,
   });
 
   factory UserModel.fromJson(Map<String, dynamic> json) {
@@ -22,6 +25,18 @@ class UserModel {
       email: json['email'],
       role: json['role'],
       company: CompanyModel.fromJson(json['company']),
+    );
+  }
+
+  factory UserModel.fromToken(String token) {
+    Map<String, dynamic> payload = JwtDecoder.decode(token);
+    return UserModel(
+      id: payload['id'] ?? 0,
+      username: payload['username'] ?? payload['username'] ?? 'User',
+      email: payload['email'] ?? payload['sub'] ?? '',
+      role: payload['role'] ?? 'USER',
+      company: CompanyModel(id: payload['companyId'] ?? 0, name: '', cnpj: ''),
+      token: token,
     );
   }
 }
